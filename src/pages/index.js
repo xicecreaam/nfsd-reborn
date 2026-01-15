498c8643e1a46e27dcc92b02dc5f88c2db0b0bbb
cataresk 😇 <codqg@icecream.home>
2026-01-15 22:51:28 +0100

    Content update

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,13 +25,13 @@ export default function Home() {
           </h2>
           <ul style={{ listStyleType: 'none', padding: 0, marginTop: '1rem' }}>
             <li style={{ marginBottom: '0.5rem' }}>
-              (WIP) <Link to="/ug1" style={{ color: '#d94d4c' }}>Need for Speed: Underground 1</Link> by <Link to="https://www.speedrun.com/users/LokyZen" style={{ color: '#d94d4c' }}>LokyZen </Link> (FG)
+              (WIP) <Link to="/ug1" style={{ color: '#d94d4c' }}>Need for Speed: Underground 1</Link> by <Link to="https://www.speedrun.com/users/LokyZen" style={{ color: '#d94d4c' }}>LokyZen </Link>
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
               (WIP) <Link to="/ug2" style={{ color: '#d94d4c' }}>Need for Speed: Underground 2</Link> by <Link to="https://www.speedrun.com/users/SlivenKage" style={{ color: '#d94d4c' }}>SlivenKage</Link>
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
-              <Link to="/mw05" style={{ color: '#d94d4c' }}>Need for Speed: Most Wanted (2005)</Link> by <Link to="https://www.speedrun.com/users/SlivenKage" style={{ color: '#d94d4c' }}>SlivenKage</Link> and IceCream
+              <Link to="/mw05" style={{ color: '#d94d4c' }}>Need for Speed: Most Wanted (2005)</Link> by <Link to="https://www.speedrun.com/users/SlivenKage" style={{ color: '#d94d4c' }}>SlivenKage</Link> and <Link to="https://www.speedrun.com/users/icecreaam" style={{ color: '#d94d4c' }}></Link>IceCream
             </li>
             <li style={{ marginBottom: '0.5rem' }}>
               (WIP) <Link to="/c" style={{ color: '#d94d4c' }}>Need for Speed: Carbon</Link> by <Link to="https://web.archive.org/web/20250422161151/https://www.speedrun.com/users/Artacer" style={{ color: '#d94d4c' }}>Artacer</Link> and  <Link to="https://www.speedrun.com/users/Cousin" style={{ color: '#d94d4c' }}>Cousin</Link>
